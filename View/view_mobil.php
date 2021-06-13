5519fadd65efc8f18bb34139b6d6f2d36a977dbf
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-06-14 02:11:11 +0700

    Add files via upload

---
+++ View/view_mobil.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mobil</title>
+    <style>
+        body{
+            margin: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Daftar Mobil</h1>
+    <table border="2" cellpadding="15" cellspacing="0" style="text-align: center;">
+        <tr>
+            <td>No</td>
+            <td>Plat Nomor</td>
+            <td>Nama</td>
+            <td>Merk</td>
+            <td>Harga</td>
+        </tr>
+        
+        <?php
+        $i = 1;
+        while($row = $this->modelMobil->fetch($data)){
+        
+            echo
+            "<tr>
+                <td>$i</td>
+                <td>$row[0]</td>
+                <td>$row[1]</td>
+                <td>$row[2]</td>
+                <td>$row[3]</td>
+            </tr>";
+        $i++;
+        }
+        ?>
+        
+    </table>
+</body>
+</html>
