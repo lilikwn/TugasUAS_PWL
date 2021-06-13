bace70b112889f51cfd8959065397c403ab049fc
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-06-14 02:12:45 +0700

    Create controller.php

---
+++ Controller/controller.php
@@ -0,0 +1,27 @@
+<?php 
+    include_once 'model/modelMobil.php';
+    include_once 'model/modelPelanggan.php';
+    include_once 'model/modelTransaksi.php';
+
+    class Controller{
+        public $modelMobil;
+        public $modelPelanggan;
+        public $modelTransaksi;
+
+        function __construct(){
+			$this->modelMobil = new modelMobil();
+
+        }
+    
+        function index(){
+            include 'view/view_home.php';
+        }
+        
+        function ViewMobil(){
+            $data = $this->modelMobil->selectAllMobil();
+            include 'view/view_mobil.php';
+        }
+    
+    }
+
+?>
